cf2709b9ebf1c42d8d698d1e509c9fd2323c4d44
arrizvi <decentboy.aliraza@gmail.com>
2021-05-23 12:44:59 +0500

    new changes effected

--- TableOrder/app/src/main/java/com/example/tableorder/MainActivity.java
+++ TableOrder/app/src/main/java/com/example/tableorder/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<TablesClass> exampleList=new ArrayList<>();
-        exampleList.add(new TablesClass(R.drawable.ic_android,"1","5","1"));
-        exampleList.add(new TablesClass(R.drawable.ic_android,"2","8","1"));
+        exampleList.add(new TablesClass(R.drawable.table,"1","5","1"));
+        exampleList.add(new TablesClass(R.drawable.table,"2","8","1"));
         exampleList.add(new TablesClass(R.drawable.ic_launcher_background,"3","6","2"));
         mRecyclerView=findViewById(R.id.recclerView);
         mRecyclerView.setHasFixedSize(true);
